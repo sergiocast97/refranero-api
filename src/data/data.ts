7d17ed27ccd24038348929d4b8c59b23a06d4156
Sergio <sergiocast97@gmail.com>
2025-04-07 18:14:30 +0100

    feat: add initial data

---
+++ src/data/data.ts
@@ -0,0 +1,21 @@
+interface Refran {
+  id: number;
+  spanish: string;
+  english: string;
+  spanglish: string;
+}
+
+export const refranero: Refran[] = [
+  {
+    id: 0,
+    spanish: 'Hola qué tal?',
+    english: 'Hey how you doing?',
+    spanglish: 'Hello what about?',
+  },
+  {
+    id: 1,
+    spanish: 'Más sabe el diablo por viejo que por diablo',
+    spanglish: 'The devil knows more for being old than for being the devil',
+    english: 'With ages comes wisdom',
+  },
+];
