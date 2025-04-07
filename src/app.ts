668d33b3af5263ffbfc17235ad38b11c95e785fa
Sergio <sergiocast97@gmail.com>
2025-04-07 20:58:10 +0100

    chore: remove boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
 import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
+import api from './refranero/routes';
 
 require('dotenv').config();
 
@@ -16,13 +15,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: 'Hello world',
-  });
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello world' });
 });
 
-app.use('/api/v1', api);
+app.get('/ping', (req: Request, res: Response) => {
+  res.json({ message: 'pong' });
+});
+
+app.use('/api/', api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 
-import ErrorResponse from './interfaces/ErrorResponse';
+interface MessageResponse {
+  message: string;
+}
+
+interface ErrorResponse extends MessageResponse {
+  stack?: string;
+}
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
@@ -9,7 +15,12 @@ export function notFound(req: Request, res: Response, next: NextFunction) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response<ErrorResponse>,
+  next: NextFunction
+) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
